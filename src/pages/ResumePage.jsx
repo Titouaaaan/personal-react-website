0c6967f0399d92063d228d23ca8bd99d5c7fa46d
Titouaaaan <titouanguerin@gmail.com>
2024-10-28 11:18:05 +0100

    actually fixed the download button

--- src/pages/ResumePage.jsx
+++ src/pages/ResumePage.jsx
@@ -3,37 +3,15 @@ import NavigationBar from '../NavigationBar';
 import { Button } from "antd";
 
 export const DownloadLink = ({ url, fileName }) => {
-    const handleDownload = () => {
-      fetch(url)
-        .then((response) => response.blob())
-        .then((blob) => {
-          const url = window.URL.createObjectURL(new Blob([blob]));
-          const link = document.createElement("a");
-          link.href = url;
-          link.download = fileName || "downloaded-file";
-          document.body.appendChild(link);
-  
-          link.click();
-  
-          document.body.removeChild(link);
-          window.URL.revokeObjectURL(url);
-        })
-        .catch((error) => {
-          console.error("Error fetching the file:", error);
-        });
-    };
-  
     return (
-      <div>
-        <Button type="primary" onClick={handleDownload}>
-          Download Resume
-        </Button>
-      </div>
+      <a href={url} download={fileName}>
+            <Button type="primary">Download Resume</Button>
+        </a>
     );
   };
 
 function ResumePage() {
-    const downloadUrl = '../assets/Titouan_Guerin_Resume_3.0.pdf'
+    const downloadUrl = "/Titouan_Guerin_Resume.pdf";
     return (
         <div>
             <NavigationBar/>
